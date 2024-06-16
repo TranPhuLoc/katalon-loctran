229ec05c445335188c1d0e2ba8f6dfd03e868ced
Loc.PhuTran <Loc.PhuTran@DESKTOP-KNAU10Q>
2024-06-16 23:32:22 +0700

    Try to run test suite

---
+++ Test Suites/API/Test Suite 01.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite 01</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>df20e50b-9d90-46c2-96d1-fa4f26e0e4b3</testSuiteGuid>
+   <testCaseLink>
+      <guid>ba35c218-5d44-4854-86fb-65802c008c08</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API/API01</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
