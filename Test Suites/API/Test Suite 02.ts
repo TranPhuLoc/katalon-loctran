5568bacbe9890cc157ba4a2fa6fe32d64ee88963
Loc.PhuTran <Loc.PhuTran@DESKTOP-KNAU10Q>
2024-06-17 00:30:50 +0700

    Create new test suite collection

---
+++ Test Suites/API/Test Suite 02.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite 02</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>bddc122a-2dbc-46d5-92ce-0821abab0130</testSuiteGuid>
+   <testCaseLink>
+      <guid>69267c7d-e5b8-4ada-b2ec-834fe6a23fe8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/WEB/WEB02</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
