ac33a001350ad8a3420a7b1132a85f35adac1ad3
Loc.PhuTran <Loc.PhuTran@DESKTOP-KNAU10Q>
2024-06-21 00:53:41 +0700

    Window desktop application

--- Scripts/WEB/WEB03/Script1718904991553.groovy
+++ Scripts/WEB/WEB03/Script1718904991553.groovy
@@ -17,17 +17,3 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-WebUI.openBrowser('')
-
-WebUI.closeBrowser()
-
-WebUI.openBrowser('')
-
-WebUI.navigateToUrl('https://katalon-demo-cura.herokuapp.com/')
-
-WebUI.click(findTestObject('Object Repository/Page_CURA Healthcare Service/h1_CURA Healthcare Service'))
-
-WebUI.verifyElementVisible(findTestObject('Object Repository/Page_CURA Healthcare Service/h1_CURA Healthcare Service'))
-
-WebUI.closeBrowser()
-
