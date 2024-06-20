64fe9ad26c55ad92ef60500adfed7b3010efc0cf
Loc.PhuTran <Loc.PhuTran@DESKTOP-KNAU10Q>
2024-06-21 01:05:50 +0700

    test executeiom

---
+++ Test Suites/API/Test Suite 03.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite 03</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>e25ece23-d35d-453f-afb5-498319a4e686</testSuiteGuid>
+   <testCaseLink>
+      <guid>fa521c91-8314-419a-ae2a-58bffa35510d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/WEB/WIN</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
